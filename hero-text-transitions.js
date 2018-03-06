dc4d83d17f7bc3b7073d37c83ea84fe319108528
SeanDanielsDigital <34247055+SeanDanielsDigital@users.noreply.github.com>
2018-03-06 17:17:49 -0500

    added main files

---
+++ hero-text-transitions.js
@@ -0,0 +1,9 @@
+$('.hero-slider').slick({
+    slidesToShow: 1,
+    prevArrow: $(".themes-carousel-controls .prev"),
+    nextArrow: $(".themes-carousel-controls .next"),
+    autoplay: true,
+    dots: false,
+    autoplaySpeed: 7000,
+    fade: true
+  });
